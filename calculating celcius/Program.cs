f39f5c63edb2c22fa70f65df86b6393034c13890
mwongeli129 <mwongeli129@gmail.com>
2023-10-06 12:05:16 +0300

    another commit

---
+++ calculating celcius/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main () 
+        {
+        int fahreinheit = 94;
+        //To convert temperatures in degrees Fahrenheit to Celsius, first subtract 32, then multiply by five ninths (5 / 9).
+
+            decimal celsius = (fahreinheit - 32m) * (5m/9m);
+
+        Console.WriteLine ("The temperature is " + celsius + " Celsius.");
+        
+    }
+}
+
+
+
+

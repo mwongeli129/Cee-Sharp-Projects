0c8d3dcc4b624e5281d7c34b7728420d00b2ceb0
mwongeli129 <mwongeli129@gmail.com>
2023-10-31 15:09:02 +0300

    commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class program 
+{ 
+    static void Main ()
+    { 
+        string[] fraudulentOrderIDs = { "A123" , "B456", "C789"};
+
+        Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+        Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+        Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+        fraudulentOrderIDs[0] = "F000";
+        Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+        Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+}
+} 

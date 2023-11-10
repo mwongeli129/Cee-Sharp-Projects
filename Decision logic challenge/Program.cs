ca7fda345129ef0cd929637d369895d3087737af
mwongeli129 <mwongeli129@gmail.com>
2023-11-10 10:54:41 +0300

    granting access based on level

---
+++ Decision logic challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains ("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, super Admin user. ");
+    }
+    else 
+    {
+        Console.WriteLine("Welcome, Admin user. ");
+    }
+}
+else if (permission.Contains ("Manager"))
+{ 
+ if (level >= 20 )
+{
+    Console.WriteLine ( "Contact an Admin for access. " );
+}
+else 
+{
+    Console.WriteLine( "You don't have sufficient privileges. ");
+}
+ }
+else
+{
+    Console.WriteLine("You do not have sufficient privileges. ");
+}
+
